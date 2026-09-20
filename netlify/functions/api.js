0cf9bb0376750101c410e6d842435e3dde1631c5
Kunal77744 <Kunal77744@users.noreply.github.com>
2026-09-20 22:32:36 +0530

    Add Netlify Serverless Functions & configuration

---
+++ netlify/functions/api.js
@@ -0,0 +1,17 @@
+'use strict';
+const serverless = require('serverless-http');
+const config = require('../../server/config');
+const { createApp } = require('../../server/app');
+const { createGeminiClient } = require('../../server/llm');
+
+const llm = config.geminiApiKey
+  ? createGeminiClient({
+      apiKey: config.geminiApiKey,
+      models: config.models,
+      timeoutMs: config.llmTimeoutMs,
+    })
+  : null;
+
+const app = createApp({ llm });
+
+module.exports.handler = serverless(app);
